10c3c33a1464210878c0dbad25d025ddf486fe11
JihadH <122496693+JihadH@users.noreply.github.com>
2025-11-02 17:51:32 -0600

    Reco Update Setter & Reco Pooler
    Added list to keep current pool of recommended songs during current session and function to refresh/update when clicking onto recommendations page again.

--- playlisted/lib/recomendations.dart
+++ playlisted/lib/recomendations.dart
@@ -8,13 +8,28 @@ import 'spotify.dart';
 //to recommend the next track, we pass in the entire list of top songs and filter based on likes.
 
 class RecommendService {
+
+  // counter for number of recommendations made, making available tracks list respectively  
   int recCount = 0;
-  List<Track> _lastRecs = const [];
-  List<Track> get lastRecs => _lastRecs;
+  List<Track> availableTracks = const [];
+  
+  // setter for available tracks towards recommendation
+  void setAvailableTracks(List<Track> tracks) {
+    availableTracks = tracks;
+  }
+  
+  // callback to update liked songs in UI when recommendations are generated 
+  void Function(List<Track>)? onUpdate;
+
+  // setter for update callback
+  void setUpdate(void Function(List<Track>) callback) {
+    onUpdate = callback;
+  }
+
   //final SpotifyService _spotifyService = SpotifyService();
 
   //Future: Function to get liked songs, then find patterns in the user's liked songs
-  void getRec(List<Track> likedsongs) async {
+  void getRec(List<Track> likedSongs) async {
     //filtering and recommendation logic can be added here
   }
   //function to add a track to liked songs and increment recommendation count
